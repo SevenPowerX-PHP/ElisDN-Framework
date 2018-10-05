14ff11f873d50d6d192b6f9daf9fbc6348c137a4
splaa <splaandrey@gmail.com>
2018-10-05 15:02:33 +0200

    00:40:00 Lesson time

--- public/index.php
+++ public/index.php
@@ -6,9 +6,21 @@
 	 * Time: 14:00
 	 */
 	
-//	$name = $_GET['name'] ?? 'Guest';
-	$name = !empty($_GET['name']) ?
-		$_GET['name'] : 'Guest';
+	function getLang($default){
+		return
+			!empty($_GET['lang']) ? $_GET['lang'] :
+				(!empty($_COOKIE['lang']) ? $_COOKIE['lang']:
+					(!empty($_SESSION['lang']) ? $_SESSION['lang'] :
+						(!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?
+							substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) :
+		$default))));
+		
+		
+		
+	}
+	$name = !empty($_GET['name']) ?	$_GET['name'] : 'Guest';
+	$lang = getLang('en');
 	
 	header('X-Developer: SevenPowerX 2018');
 	echo 'Hello PHP I am ' . $name . PHP_EOL;
+	echo 'Your lang: ' . $lang . PHP_EOL;
