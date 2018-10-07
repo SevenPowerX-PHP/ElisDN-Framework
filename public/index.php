4a95d85d18eaa698d087331844a1c38a56459c6f
splaa <splaandrey@gmail.com>
2018-10-07 19:19:19 +0200

    time 1:35:00

--- public/index.php
+++ public/index.php
@@ -6,32 +6,20 @@
 	 * Time: 14:00
 	 */
 	
-	use Framework\Http\Request;
+	use Framework\Http\RequestFactory;
 	
 	chdir(dirname(__DIR__));
 	require_once "vendor/autoload.php";
 	
 	### Initialization
 	
-//	$request = new Request($_GET, $_POST);
-/*	$request = new Request();
-	$request->withQueryParams($_GET);
-	$request->withParsedBody($_POST);*/
-	
-// Чтобы воспользоватся цепочкой нужно в сетерах вернуть сам обьект
-//  return this;
-$request = (new Request())
-	->withQueryParams($_GET)
-	->withParsedBody($_POST);
-	
-//	$request->setParserBody(json_decode($request->getBody()));
+	$request = RequestFactory::fromGlobals();
 	
 	### Action
 	
-	$name = !empty($request->getQueryParams()['name']) ? $request->getQueryParams()['name'] : 'Guest';
-	$lang = 'en';
-	//	$lang = !empty($request->getParseBody()) ? $request->getParseBody() : 'en';
+	$name = $request->getQueryParams()['name'] ?? 'Guest';
 	
 	header('X-Developer: SevenPowerX 2018');
 	echo 'Hello PHP I am ' . $name . PHP_EOL;
+	$lang ='en';
 	echo 'Your lang: ' . $lang . PHP_EOL;
