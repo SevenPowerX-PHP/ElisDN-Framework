300161e75b5158b21808e44fedfaf8d92e9287ba
splaa <splaandrey@gmail.com>
2018-10-07 18:37:07 +0200

    Added with methods end return this. time: 11:25:00

--- public/index.php
+++ public/index.php
@@ -6,21 +6,6 @@
 	 * Time: 14:00
 	 */
 	
-	/*function getLang(array $get, array $cookie, array $session, array $server, $default){
-		return
-			!empty($get['lang']) ? $get['lang'] :
-				(!empty($cookie['lang']) ? $cookie['lang']:
-					(!empty($session['lang']) ? $session['lang'] :
-						(!empty($server['HTTP_ACCEPT_LANGUAGE']) ?
-							substr($server['HTTP_ACCEPT_LANGUAGE'], 0, 2) :
-		$default)));
-		
-		
-		
-	}
-	
-	session_start();*/
-	
 	use Framework\Http\Request;
 	
 	chdir(dirname(__DIR__));
@@ -28,16 +13,24 @@
 	
 	### Initialization
 	
-	$request = new Request($_GET, $_POST);
+//	$request = new Request($_GET, $_POST);
+/*	$request = new Request();
+	$request->withQueryParams($_GET);
+	$request->withParsedBody($_POST);*/
+	
+// Чтобы воспользоватся цепочкой нужно в сетерах вернуть сам обьект
+//  return this;
+$request = (new Request())
+	->withQueryParams($_GET)
+	->withParsedBody($_POST);
+	
+//	$request->setParserBody(json_decode($request->getBody()));
 	
 	### Action
 	
 	$name = !empty($request->getQueryParams()['name']) ? $request->getQueryParams()['name'] : 'Guest';
-	/*
-		$name = !empty($_GET['name']) ?	$_GET['name'] : 'Guest';
-		$lang = getLang($_GET, $_COOKIE, $_SESSION, $_SERVER,'en');
-	*/
-		$lang = 'en';
+	$lang = 'en';
+	//	$lang = !empty($request->getParseBody()) ? $request->getParseBody() : 'en';
 	
 	header('X-Developer: SevenPowerX 2018');
 	echo 'Hello PHP I am ' . $name . PHP_EOL;
--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -11,20 +11,32 @@
 	
 	class Request
 	{
-		private $queryParams;
+		private $queryParams = [];
 		private $parsedBody;
 		
-		/**
-		 * Request constructor.
-		 * @param array $queryParams
-		 * @param array|null $parsedBody
-		 */
-		public function __construct(array $queryParams = [], array $parsedBody = null)
+		public function withQueryParams(array $query)
+		{
+			$this->queryParams = $query;
+			return $this;
+		}
+		
+		public function withParsedBody($data)
+		{
+			$this->parsedBody = $data;
+			return $this;
+		}
+		
+//		/**
+//		 * Request constructor.
+//		 * @param array $queryParams
+//		 * @param array|null $parsedBody
+//		 */
+		/*public function __construct(array $queryParams = [], array $parsedBody = null)
 		{
 			$this->queryParams = $queryParams;
 			$this->parsedBody = $parsedBody;
 		}
-		
+		*/
 		public function getQueryParams(): array {
 			return $this->queryParams;
 		}
