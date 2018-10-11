40d8064d79ddb77999e2c9bc1e0302ccce822084
splaa <splaandrey@gmail.com>
2018-10-11 07:46:39 +0200

    Added package zendframework/zend-httphandlerrunner use Zend\HttpHandlerRunner\Emitter\SapiEmitter;

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
 	 */
 	
 	use Zend\Diactoros\Response\HtmlResponse;
-	use Zend\Diactoros\Response\SapiEmitter;
+	use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 	use Zend\Diactoros\ServerRequestFactory;
 	
 	chdir(dirname(__DIR__));
@@ -21,11 +21,36 @@
 	
 	$name = $request->getQueryParams()['name'] ?? 'Guest';
 	
-	$response = (new HtmlResponse('Hello,' . $name . '!'))
-		->withHeader('X-Developer', 'SplX');
+	$path = $request->getUri()->getPath();
+	
+	
+	/*if ($path === '/') {
+		$name = $request->getQueryParams()['name'] ?? 'Test';
+		$response = (new HtmlResponse('Hello,' . $name . '!'));
+	} elseif ($path === '/about') {
+		$response = new HtmlResponse('I am a simple site');
+	} else {
+		$response = new \Zend\Diactoros\Response\JsonResponse(['error' => 'Undefined page'], 404);
+	}*/
+	
+	$name = $request->getQueryParams()['name'] ?? 'Test';
+	$response = (new HtmlResponse('Hello,' . $name . '!'));
+	
+	### Preprocessing
+	
+//	$response = $request->withHeader('X-Developer', 'SplX');
+
+//	$response = $request->withHeader('X-Developer1', 'SplX=1');
+//	$response = $request->withoutHeader('X-Developer');
+	
+	/*	if(preg_match('#json#i', $request->getHeader('Content-Type')))
+		{
+			$request = $request->withParsedBody(json_decode($request->getBody()->getContents()));
+		}*/
 	
 	###Sending
 	
 	
 	$emitter = new SapiEmitter();
+	
 	$emitter->emit($response);
